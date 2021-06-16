e360fe71ffae92cf637ee64a48f9cdce53e65901
Diana <diana.birkelbach@orbis.de>
2021-06-16 21:01:28 +0200

    first commit

---
+++ LookupTest/index.ts
@@ -0,0 +1,112 @@
+/* eslint-disable quotes */
+/* eslint-disable space-before-blocks */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable indent */
+/* eslint-disable semi */
+/* eslint-disable no-mixed-spaces-and-tabs */
+/* eslint-disable no-tabs */
+/* eslint-disable no-undef */
+import { IInputs, IOutputs } from './generated/ManifestTypes'
+
+export class LookupTest implements ComponentFramework.StandardControl<IInputs, IOutputs> {
+	private val: any[] = [];
+	private container:HTMLDivElement;
+	private content: HTMLSpanElement;
+
+	/**
+	 * Empty constructor.
+	 */
+	// eslint-disable-next-line no-useless-constructor
+	constructor () {
+
+	}
+
+	private logMe(lookup1: ComponentFramework.PropertyTypes.Property, lookup2: ComponentFramework.PropertyTypes.Property){
+		console.group();
+		console.log("values", lookup1.raw, lookup2.raw);	
+		
+		console.log(`Lookup configuration:`, (lookup1 as any).getLookupConfiguration(), (lookup2 as any).getLookupConfiguration());
+		console.group("1. Display Search Box")		
+		console.log("disableQuickFind: ", (lookup1 as any).disableQuickFind, (lookup2 as any).disableQuickFind);
+		console.groupEnd();
+		console.group("2. View Selector");
+		console.log("enableViewPicker: ", (lookup1 as any).enableViewPicker, (lookup2 as any).enableViewPicker); 
+		console.groupEnd();
+		console.group("3. Display Views");
+		console.log("availableViewIds: ", (lookup1 as any).availableViewIds, (lookup2 as any).availableViewIds); 
+		console.log("availableViewNames: ", (lookup1 as any).availableViewNames, (lookup2 as any).availableViewNames); 
+		console.groupEnd();
+		console.group("4. DefaultView");
+		console.log("getDefaultViewId: ", (lookup1 as any).getDefaultViewId(), (lookup2 as any).getDefaultViewId()); 
+		console.log("getTitle: ", (lookup1 as any).getTitle(), (lookup2 as any).getTitle()); 
+		console.groupEnd();
+		console.group("5. Filter on Related Lookup");
+		console.log("filterRelationshipName: ", (lookup1 as any).filterRelationshipName, (lookup2 as any).filterRelationshipName); 
+		console.log("dependentAttributeName: ", (lookup1 as any).dependentAttributeName, (lookup2 as any).dependentAttributeName); 		
+		console.log("dependentAttributeType: ", (lookup1 as any).dependentAttributeType, (lookup2 as any).dependentAttributeType); 
+		console.groupEnd()
+		console.group("6. Users can turn off filter")		
+		console.log("allowFilterOff: ", (lookup1 as any).allowFilterOff, (lookup2 as any).allowFilterOff);
+		console.groupEnd();		
+		console.groupEnd();
+	}
+	
+
+	/**
+	 * Used to initialize the control instance. Controls can kick off remote server calls and other initialization actions here.
+	 * Data-set values are not initialized here, use updateView.
+	 * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to property names defined in the manifest, as well as utility functions.
+	 * @param notifyOutputChanged A callback method to alert the framework that the control has new outputs ready to be retrieved asynchronously.
+	 * @param state A piece of data that persists in one session for a single user. Can be set at any point in a controls life cycle by calling 'setControlState' in the Mode interface.
+	 * @param container If a control is marked control-type='standard', it will receive an empty div element within which it can render its content.
+	 */
+	public init (context: ComponentFramework.Context<IInputs>, notifyOutputChanged: () => void, state: ComponentFramework.Dictionary, container:HTMLDivElement): void {
+	  this.container = container;
+	  const button = document.createElement('button');
+	  button.textContent = 'Click me';
+	  button.addEventListener('click', () => {
+		 (context.parameters.sampleProperty as any).runPreSearch();
+		 console.log("after presearch 1", context.parameters.sampleProperty);
+		 const ret2 = (context.parameters.secondProperty as any).runPreSearch();
+		 console.log("after preSearch 2", context.parameters.secondProperty);
+	     notifyOutputChanged()
+	  });
+	  container.appendChild(button);
+	  const button1 = document.createElement('button');
+	  button1.textContent = 'openRecord';
+	  button1.addEventListener('click', () => {
+		  if (context.parameters.sampleProperty.raw?.length > 0){
+		 		(context.parameters.sampleProperty as any).openDatasetItem(context.parameters.sampleProperty.raw[0]);
+		  }
+	  });
+	  container.appendChild(button1);
+
+	  this.content = document.createElement('span');
+	  container.appendChild(this.content);
+	  this.logMe(context.parameters.sampleProperty, context.parameters.secondProperty);	  
+	}
+
+	/**
+	 * Called when any value in the property bag has changed. This includes field values, data-sets, global values such as container height and width, offline status, control metadata values such as label, visible, etc.
+	 * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to names defined in the manifest, as well as utility functions
+	 */
+	public updateView (context: ComponentFramework.Context<IInputs>): void {	  	  	  
+		this.logMe(context.parameters.sampleProperty, context.parameters.secondProperty);
+	}
+
+	/**
+	 * It is called by the framework prior to a control receiving new data.
+	 * @returns an object based on nomenclature defined in manifest, expecting object[s] for property marked as “bound” or “output”
+	 */
+	public getOutputs (): IOutputs {
+	  return { sampleProperty: this.val }
+	}
+
+	/**
+	 * Called when the control is to be removed from the DOM tree. Controls should use this call for cleanup.
+	 * i.e. cancelling any pending remote calls, removing listeners, etc.
+	 */
+	public destroy (): void {
+	  // Add code to cleanup control if necessary
+	}
+}
